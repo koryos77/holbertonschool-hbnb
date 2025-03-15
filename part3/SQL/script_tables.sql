a935c9883e99add72355127ebcee99bfaffd971c
koryos77 <k.rouget21@gmail.com>
2025-03-15 17:41:14 +0100

    adding the SQL files

---
+++ part3/SQL/script_tables.sql
@@ -0,0 +1,49 @@
+-- Script for table creations for the database of HBnB
+
+-- Users table
+CREATE TABLE IF NOT EXISTS users (
+    id CHAR(36) PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255)NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    is_admin BOOLEAN DEFAULT FALSE
+);
+
+-- Places table
+CREATE TABLE IF NOT EXISTS places (
+    id CHAR(36) PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    latitude FLOAT NOT NULL,
+    Longitude FLOAT NOT NULL,
+    owned_id CHAR(36) NOT NULL,
+    FOREIGN KEY (owned_id) REFERENCES user(id)
+);
+
+-- Review table
+CREATE TABLE IF NOT EXISTS reviews (
+    id CHAR(36) PRIMARY KEY,
+    text TEXT NOT NULL,
+    rating INTEGER NOT NULL,
+    user_id CHAR(36) NOT NULL,
+    place_id CHAR(36) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (place_id) REFERENCES places(id)
+);
+
+-- Amenity table
+CREATE TABLE IF NOT EXISTS amenities (
+    id CHAR(36), PRIMARY KEY,
+    name VARCHAR(255) UNIQUE
+);
+
+-- Place_amenity table
+CREATE TABLE IF NOT EXISTS place_amenity(
+    place_id VARCHAR(36) NOT NULL,
+    amenity_id VARCHAR(36) NOT NULL,
+    PRIMARY KEY (place_id, amenity_id),
+    FOREIGN KEY (place_id) REFERENCES places(id),
+    FOREIGN KEY (amenity_id) REFERENCES amenities(id)
+);
